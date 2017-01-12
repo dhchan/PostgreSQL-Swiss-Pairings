99f66e1e7831b4cddfcf981b63b33be7ea889b2a
Daniel Chan <daniel_chan@comcast.net>
2017-01-12 13:40:17 -0800

    Update tournament_test.py

--- tournament_test.py
+++ tournament_test.py
@@ -3,6 +3,8 @@
 # Test cases for tournament.py
 # These tests are not exhaustive, but they should cover the majority of cases.
 #
+# Test by Udacity
+#
 # If you do add any of the extra credit options, be sure to add/modify these test cases
 # as appropriate to account for your module's added functionality.
 
