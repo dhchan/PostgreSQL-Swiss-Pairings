7cd39e87cf84669e295c206d072cba252d669e8b
Daniel Chan <daniel_chan@comcast.net>
2017-01-12 13:41:37 -0800

    Updated Description

--- tournament.sql
+++ tournament.sql
@@ -6,7 +6,7 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
---by Udacity
+--Test Queries. by Daniel Chan
 
 --vagrant => psql
 --\c tournament   #to connect to the tournament database
