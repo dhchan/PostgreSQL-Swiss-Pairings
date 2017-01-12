d30e202cecc0a2d4bdda0fe046292356fdc24822
Daniel Chan <daniel_chan@comcast.net>
2017-01-12 13:39:31 -0800

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -6,6 +6,8 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+--by Udacity
+
 --vagrant => psql
 --\c tournament   #to connect to the tournament database
 
