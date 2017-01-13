1a50a78e4ee0ad549d54e2c2ffb9d78f0f8d701b
Daniel Chan <daniel_chan@comcast.net>
2017-01-12 21:00:42 -0800

    Corrections

--- tournament.sql
+++ tournament.sql
@@ -6,8 +6,6 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
---Test Queries. by Daniel Chan
-
 --vagrant => psql
 --\c tournament   #to connect to the tournament database
 
@@ -18,33 +16,48 @@ CREATE DATABASE tournament;
 DROP TABLE IF EXISTS Players;
 CREATE TABLE Players (
   id serial primary key,
-  name text,
-  wins int,
-  matches int);
+  name text);
   
 DROP TABLE IF EXISTS Matches;
 CREATE TABLE Matches (
   match_id serial,
-  player1 int references Players (id),
-  player2 int references Players (id),
-  winner text);
+  winner int references Players (id),
+  loser int references Players (id));
   
   
-INSERT INTO Players (name, wins, matches) VALUES ('A', 3, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('B', 2, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('C', 1, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('D', 0, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('E', 3, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('F', 0, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('G', 1, 3);
-INSERT INTO Players (name, wins, matches) VALUES ('H', 2, 3);
+INSERT INTO Players (name) VALUES ('A');
+INSERT INTO Players (name) VALUES ('B');
+INSERT INTO Players (name) VALUES ('C');
+INSERT INTO Players (name) VALUES ('D');
+INSERT INTO Players (name) VALUES ('E');
+INSERT INTO Players (name) VALUES ('F');
+INSERT INTO Players (name) VALUES ('G');
+INSERT INTO Players (name) VALUES ('H');
 
-SELECT COUNT (*) from Players;
+INSERT INTO Matches (winner, loser) VALUES (1, 2);
+INSERT INTO Matches (winner, loser) VALUES (1, 3);
+INSERT INTO Matches (winner, loser) VALUES (1, 4);
+INSERT INTO Matches (winner, loser) VALUES (1, 5);
+INSERT INTO Matches (winner, loser) VALUES (2, 1);
 
-SELECT id, name FROM Players ORDER BY wins DESC LIMIT 2 OFFSET 0;
 
-SELECT id, name FROM Players ORDER BY wins DESC LIMIT 2 OFFSET 2;
+CREATE OR REPLACE VIEW WinCount as
+    SELECT Players.id, Players.name, count(Matches.winner) as wins
+    FROM Players left join Matches ON Players.id = Matches.winner
+    GROUP BY Players.id
+    ORDER BY id;
+  
+CREATE OR REPLACE VIEW LossCount as
+    SELECT Players.id, Players.name, count(Matches.loser) as losses
+    FROM Players left join Matches ON Players.id = Matches.loser
+    GROUP BY Players.id
+    ORDER BY id;
+    
+SELECT WinCount.id, WinCount.name, WinCount.wins, WinCount.wins + LossCount.losses as Matches
+    FROM WinCount join LossCount ON WinCount.id = LossCount.id;
 
-SELECT id, name FROM Players ORDER BY wins DESC LIMIT 2 OFFSET 4;
-
-SELECT id, name FROM Players ORDER BY wins DESC LIMIT 2 OFFSET 6;
+SELECT Players.id, Players.name, Count(Matches.winner) as wins
+      FROM Players left join Matches on Players.id = Matches.winner
+      GROUP BY Players.id
+      ORDER BY wins DESC;
+    
